4eb8a9cf19e6dc76bc1c3ca9f5b5e23ed8e22f37
Pong <samfunghp@gmail.com>
2021-07-19 22:17:20 +0800

    fix(): eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json", "./tsconfig.eslint.json"],
+  },
+  plugins: ["@typescript-eslint/eslint-plugin", "jest", "prettier"],
+  extends: [
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  root: true,
+  env: {
+    commonjs: true,
+    es6: true,
+    node: true,
+    browser: false,
+  },
+  globals: {
+    strapi: true,
+    request: true,
+    mongod: true,
+    users: true,
+    regions: true,
+    categories: true,
+  },
+  rules: {
+    "no-redeclare": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-redeclare": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      { ignoreRestSiblings: true, argsIgnorePattern: "_" },
+    ],
+  },
+};
