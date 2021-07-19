fe2b058af2558598b1ea8e7f01197fcc9dd498ef
Pong <samfunghp@gmail.com>
2021-07-19 22:18:02 +0800

    feat(): make tsup/esbuild config better

---
+++ esbuild/clean.js
@@ -0,0 +1,32 @@
+const fs = require("fs/promises");
+const glob = require("globby");
+
+/**
+ * @param {string[]} patterns
+ */
+const clean = (patterns) => {
+  /**
+   * @type {import('esbuild').Plugin}
+   */
+  const plugin = {
+    name: "clean",
+    setup(build) {
+      const { outdir } = build.initialOptions;
+      if (!outdir) return;
+
+      const removeFiles = async () => {
+        const files = await glob(patterns, {
+          cwd: outdir,
+          absolute: true,
+        });
+        await Promise.all(files.map((file) => fs.unlink(file)));
+      };
+
+      build.onStart(removeFiles);
+    },
+  };
+
+  return plugin;
+};
+
+module.exports = { clean };
--- tsup.config.js
+++ tsup.config.js
@@ -1,45 +1,21 @@
 // @ts-check
-const path = require('path');
+const { clean } = require("./esbuild/clean");
+const { resolvePathAlias } = require("./esbuild/resolvePathAlias");
 
-const outDir = 'app';
-
-/** @type {Map<string, string>} */
-const cache = new Map();
-
-/**
- * @type {import('esbuild').Plugin}
- */
-const resolvePathAlias = {
-  name: 'resolve-path-alias',
-  setup(build) {
-    build.onEnd((result) => {
-      result.outputFiles = result.outputFiles.map((args) => {
-        let alias = cache.get(args.path);
-        if (!alias) {
-          alias = path.relative(path.dirname(args.path), outDir) || '.';
-          cache.set(args.path, alias);
-        }
-        return {
-          ...args,
-          text: args.text.replace(/[require\(|from ]['|"]@\//gm, (s) =>
-            s.replace('@/', `${alias}/`)
-          ),
-        };
-      });
-    });
-  },
-};
+const outDir = "app";
 
 /**
  * @type {import("tsup").Options}
  */
 module.exports = {
-  entryPoints: ['strapi/**/*.{js,ts}'],
-  esbuildPlugins: [resolvePathAlias],
-  skipNodeModulesBundle: true,
+  entryPoints: ["strapi/**/*.ts", "!**/*.d.ts"],
+  esbuildPlugins: [
+    clean(["**/*", "!**/*.d.ts", "!build/*", "!.cache"]),
+    resolvePathAlias(outDir),
+  ],
   keepNames: true,
   splitting: false,
   clean: false,
   outDir,
-  onSuccess: 'node postbuild.js',
+  onSuccess: "node postbuild.js",
 };
