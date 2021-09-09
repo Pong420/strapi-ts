b5228204ee9da8601381bd70acc63d33053c601b
Pong <samfunghp@gmail.com>
2021-09-09 23:49:45 +0800

    refactor(): create schema policy

--- strapi/schema/createSchemaPolicy.ts
+++ strapi/schema/createSchemaPolicy.ts
@@ -1,36 +1,70 @@
-import type { ObjectSchema, ValidationOptions, ValidationResult } from 'joi';
-import { mapKeys } from 'lodash';
+import _ from 'lodash';
+import File from 'formidable/lib/file';
+import type {
+  ObjectSchema,
+  AlternativesSchema,
+  ValidationOptions,
+  ValidationResult
+} from 'joi';
+
+interface CreatePolicyOptions extends ValidationOptions {
+  // merge customizer, anims for schema contain array of file
+  merge?: (objValue: any, srcValue: any) => any;
+}
+
+export const mergeWith = (...object: [any, any, any?, any?, any?]) => {
+  return _.mergeWith(...object, (objValue, srcValue) => {
+    if (_.isArray(objValue)) {
+      return objValue.concat(srcValue);
+    }
+  });
+};
 
 export function createSchemaPolicy(
-  schema: ObjectSchema,
+  schema: ObjectSchema | AlternativesSchema,
   type: 'body' | 'query',
-  options?: ValidationOptions
+  { merge = mergeWith, ...options }: CreatePolicyOptions = {}
 ) {
   async function policy(ctx: KoaContext<any>, next: KoaNext) {
     let result: ValidationResult;
-    const files = mapKeys(ctx.request.files, (_, key) =>
+    const files = _.mapKeys(ctx.request.files, (_, key) =>
       // strapi suggested to prefix all files with `files`. For example,`files.cover`
       key.replace(/^files\./, '')
     );
 
     if (ctx.is('multipart')) {
+      let data: unknown;
+
       try {
-        result = schema.validate(
-          { ...JSON.parse(ctx.request.body.data || '{}'), ...files },
-          options
-        );
-
-        for (const key in files) {
-          const { [key]: file, ...rest } = result.value;
-          result.value = rest;
-
-          // for futre if the file has transformation
-          files[key] = file;
-        }
+        const raw = ctx.request.body.data || '{}';
+        data = JSON.parse(raw);
       } catch (error) {
         return ctx.badRequest(
           `Invalid 'data' field. 'data' should be a valid JSON.`
         );
+      }
+
+      result = schema.validate(
+        // merge for schema contains array of files
+        merge(data, files),
+        options
+      );
+
+      // Extract the files
+      // Since the value could be converted, should not use `result.value = JSON.parse(raw);`
+      for (const key in files) {
+        const { [key]: file, ...rest } = result.value;
+        result.value = rest;
+
+        if (Array.isArray(file)) {
+          const [_file, payload] = _.partition(file, f => f instanceof File);
+          if (payload.length) {
+            result.value[key] = payload;
+          }
+          files[key] = _file;
+        } else {
+          files[key] = file;
+        }
       }
     } else {
       result = schema.validate(ctx.request[type], options);
