b0f0f7ded9f8f1fcaa860964be8d0003094bda7c
Pong <samfunghp@gmail.com>
2021-09-17 23:50:43 +0800

    feat(): upgreade package

--- preinstall.js
+++ preinstall.js
@@ -1,3 +1,4 @@
+// @ts-check
 const fs = require('fs');
 
 /**
@@ -8,5 +9,34 @@ const files = ['app/package.json'];
 for (const file of files) {
   if (fs.existsSync(file)) {
     fs.unlinkSync(file);
+  }
+}
+
+/** @type {import('./strapi/package.json') | undefined} */
+let strapiPkg;
+
+try {
+  strapiPkg = require('./strapi/package.json');
+} catch {
+  // error if using docker
+}
+
+if (strapiPkg) {
+  const deps = { ...strapiPkg.dependencies, ...strapiPkg.devDependencies };
+
+  if (
+    deps['jest'] !== '~26.6.3' ||
+    deps['jest-circus'] !== '~26.6.3' ||
+    deps['@types/jest'] !== '27.0.1'
+  ) {
+    throw new Error(
+      'Do not upgrade the jest packages. Since strapi limits the node version and the version is not supported by jest above 26'
+    );
+  }
+
+  if (deps['@types/pino'] !== '4.7.1') {
+    throw new Error(
+      'Do not upgrade the "@types/pino". Since the higher version is not compatible with strapi v3.'
+    );
   }
 }
