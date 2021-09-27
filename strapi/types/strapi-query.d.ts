3a2872e7ae3b2f4dd0eda4a34bf796a9edf332bc
Pong <samfunghp@gmail.com>
2021-09-27 12:00:28 +0800

    fix(): strapi populate path

--- strapi/types/strapi-query.d.ts
+++ strapi/types/strapi-query.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
 // eslint-disable-next-line
 import * as strapi from 'strapi';
 import { IQueryParam } from '@/typings';
@@ -6,6 +8,28 @@ declare module 'strapi' {
   export type Search<T> = IQueryParam<T> & {
     _q?: string;
   };
+
+  // assume only object could be poplulate
+  type PopulatePath<T> = NonNullable<AllowedNames<T, object>>;
+
+  export type NestedPopulatePaths<
+    T extends object,
+    K extends keyof T
+  > = T[K] extends object
+    ? `${Extract<K, string>}.${Extract<PopulatePath<T[K]>, string>}`
+    : never;
+
+  export type MongosePopulate<T extends object, K extends keyof T> = {
+    path: K;
+    populate?: T[K] extends object
+      ? MongosePopulate<T[K], PopulatePath<T[K]>>
+      : never;
+  };
+
+  export type Populate<T, K extends keyof T = PopulatePath<T>> =
+    | K
+    | NestedPopulatePaths<T, K>
+    | MongosePopulate<T, K>;
 
   // The response is not confirmed
   // https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#api-reference
@@ -18,15 +42,15 @@ declare module 'strapi' {
     Data = Model<DataSchema>,
     Read = Partial<ReadBase> & Search<ReadBase> & { id?: string }
   > {
-    find(query?: Read, populate?: (keyof DataSchema)[]): Promise<Data[]>;
-    findOne(query: Read, populate?: (keyof DataSchema)[]): Promise<Data>;
+    find(query?: Read, populate?: Populate<DataSchema>[]): Promise<Data[]>;
+    findOne(query: Read, populate?: Populate<DataSchema>[]): Promise<Data>;
     create(payload: Omit<Create, 'id'>): Promise<Data>;
     update(query: Read, payload: Update): Promise<Data>;
     delete(payload: Delete): Promise<unknown>;
     count(query?: Partial<Data>): Promise<number>;
     search(search: Read): Promise<Data[]>;
     countSearch(search: Read): Promise<number>;
-    model: Model<Data>['model'];
+    model: Model<Data>;
   }
 
   interface Strapi {
--- strapi/types/strapi-services.d.ts
+++ strapi/types/strapi-services.d.ts
@@ -9,28 +9,6 @@ type Files = Record<string, File | File[]>;
 declare module 'strapi' {
   // https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#services
 
-  // assume only object could be poplulate
-  type PopulatePath<T> = NonNullable<AllowedNames<T, object>>;
-
-  export type NestedPopulatePaths<
-    T extends object,
-    K extends keyof T
-  > = T[K] extends object
-    ? `${Extract<K, string>}.${Extract<PopulatePath<T[K]>, string>}`
-    : never;
-
-  export type MongosePopulate<T extends object, K extends keyof T> = {
-    path: K;
-    populate?: T[K] extends object
-      ? MongosePopulate<T[K], PopulatePath<T[K]>>
-      : never;
-  };
-
-  export type Populate<T, K extends keyof T = PopulatePath<T>> =
-    | K
-    | NestedPopulatePaths<T, K>
-    | MongosePopulate<T, K>;
-
   export interface CollectionTypeService<Data> {
     find(params: any, populate?: Populate<Data>[]): Promise<Data[]>;
     findOne(params: any, populate?: Populate<Data>[]): Promise<Data>;
