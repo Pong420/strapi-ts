239eaf7299a4ad5892d15a7134861089f83283ea
Pong <samfunghp@gmail.com>
2021-11-07 00:09:53 +0800

    fix(): packages

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -26,7 +26,7 @@ if (strapiPkg) {
   if (
     deps['jest'] !== '~26.6.3' ||
     deps['jest-circus'] !== '~26.6.3' ||
-    deps['@types/jest'] !== '27.0.1'
+    deps['@types/jest'] !== '27.0.2'
   ) {
     throw new Error(
       'Do not upgrade the jest packages. Since strapi limits the node version and the version is not supported by jest above 26'
@@ -35,7 +35,13 @@ if (strapiPkg) {
 
   if (deps['@types/pino'] !== '4.7.1') {
     throw new Error(
-      'Do not upgrade the "@types/pino". Since the higher version is not compatible with strapi v3.'
+      'Do not upgrade "@types/pino". Since the higher version is not compatible with strapi v3.'
+    );
+  }
+
+  if (!/5\.\d+\.\d+/.test(deps['mongoose'])) {
+    throw new Error(
+      'Do not upgrade "mongoose" version to 6. Since the types of mongoose is difference in v6 but strapi is using mongoose version "5.10.8"'
     );
   }
 }
