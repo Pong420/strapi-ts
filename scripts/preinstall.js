be3da40063e4b2248d78343759bac37fff75d6e9
Pong <samfunghp@gmail.com>
2021-12-09 23:15:13 +0800

    fix(): preinstall

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,17 +1,3 @@
-// @ts-check
-const fs = require('fs');
-
-/**
- * remove the `app/package.json`, make sure old packages will not be installed
- */
-const files = ['app/package.json'];
-
-for (const file of files) {
-  if (fs.existsSync(file)) {
-    fs.unlinkSync(file);
-  }
-}
-
 /** @type {import('../strapi/package.json') | undefined} */
 let strapiPkg;
 
