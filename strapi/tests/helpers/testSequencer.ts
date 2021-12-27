c80a4c9d6a172fa4afab30ad8ee88ca65952b50f
Pong <samfunghp@gmail.com>
2021-12-27 12:12:03 +0800

    fix(): test sequencer

--- strapi/tests/helpers/testSequencer.ts
+++ strapi/tests/helpers/testSequencer.ts
@@ -1,5 +1,6 @@
 import Sequencer from '@jest/test-sequencer';
 import { Test } from 'jest-runner';
+import { uniqBy } from 'lodash';
 
 /**
  * sort tests order if necessary
@@ -14,16 +15,17 @@ const orders = ['category', 'product'];
 export default class CustomSequencer extends Sequencer {
   sort(tests: Test[]) {
     // Test structure information
-    // https://github.com/facebook/jest/blob/6b8b1404a1d9254e7d5d90a8934087a9c9899dab/packages/jest-runner/src/types.ts#L17-L21
-    const copyTests = Array.from(tests);
+    // https://github.dev/facebook/jest/blob/6b8b1404a1d9254e7d5d90a8934087a9c9899dab/packages/jest-runner/src/types.ts#L17-L21
 
-    return copyTests.sort((testA, testB) => {
-      const nameA = getFilenameFromPath(testA.path);
-      const nameB = getFilenameFromPath(testB.path);
-      if (orders.includes(nameA) && orders.includes(nameB)) {
-        return orders.indexOf(nameA) - orders.indexOf(nameB);
-      }
-      return nameA.localeCompare(nameB);
-    });
+    const map = tests.reduce((acc, test) => {
+      return { ...acc, [getFilenameFromPath(test.path)]: test };
+    }, {} as Record<string, Test>);
+
+    const copyTests: Test[] = uniqBy(
+      [...orders.map(path => map[path]).filter(Boolean), ...Array.from(tests)],
+      test => test.path
+    );
+
+    return copyTests;
   }
 }
